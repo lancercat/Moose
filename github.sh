a96e4ce46a3595a56ceab4d4aa3a65c9912f79fe
lancercat <lasercat@gmx.us>
2024-05-14 14:41:09 +0200

    Delete github.sh

--- github.sh
+++
@@ -1,5 +0,0 @@
-git add --update;
-git add .;
-git commit -m "meow";
-GIT_SSH_COMMAND="ssh -i /home/lasercat/.ssh/proj300" git pull github master --rebase
-GIT_SSH_COMMAND="ssh -i /home/lasercat/.ssh/proj300" git push github master
